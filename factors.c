ff414638d0159d4c25273f096f6fa7c912069e6c
Aishah <maishah2540@gmail.com>
2023-08-06 05:31:26 -0700

    Updated te code to cater for long numbers

--- factors.c
+++ factors.c
@@ -1,38 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void factorize(int number) {
-    int factor1, factor2;
+void factorize(long long int number) {
+    long long int factor1, factor2;
 
-    for (int i = 2; i <= number / 2; ++i) {
+    for (long long int i = 2; i <= number / 2; ++i) {
         if (number % i == 0) {
             factor1 = i;
             factor2 = number / i;
-            printf("%d=%d*%d\n", number, factor1, factor2);
+            printf("%lld=%lld*%lld\n", number, factor1, factor2);
             return;
         }
     }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2)
-	    exit(EXIT_FAILURE);
+    if (argc != 2) {
+        printf("Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
 
     FILE *file;
     char *filename = argv[1];
-    int number;
+    long long int number;
 
-    /* Open the file in read mode*/
+    /* Open the file in read mode */
     file = fopen(filename, "r");
-    if (file == NULL)
-	    exit(EXIT_FAILURE);
-
-    /* Read numbers from the file until the end is reached */
-    while (fscanf(file, "%d", &number) != EOF) {
-        factorize(number);
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;
     }
 
-    // Close the file
+    /* Read numbers from the file until the end is reached */
+    while (fscanf(file, "%lld", &number) != EOF) {
+        factorize(number);
+
+        /* Skip the newline character after reading the number */
+        char c;
+        do {
+            c = fgetc(file);
+        } while (c != '\n' && c != EOF);
+    }
+
+    /* Close the file */
     fclose(file);
 
     return 0;
